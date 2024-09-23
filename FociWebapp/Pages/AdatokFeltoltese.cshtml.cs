63bff1bdc5540beaa5913106b0ff465a8a021aa6
= <=>
2024-09-23 13:49:12 +0200

    1313

--- FociWebapp/Pages/AdatokFeltoltese.cshtml.cs
+++ FociWebapp/Pages/AdatokFeltoltese.cshtml.cs
@@ -1,3 +1,4 @@
+using FociWebapp.Models;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 
@@ -6,9 +7,11 @@ namespace FociWebapp.Pages
     public class AdatokFeltolteseModel : PageModel
     {
         private readonly IWebHostEnvironment _env;
-        public AdatokFeltolteseModel(IWebHostEnvironment env)
+        private readonly FociDbContext _context;
+        public AdatokFeltolteseModel(IWebHostEnvironment env, FociDbContext context)
         {
             _env = env;
+            _context = context;
         }
         [BindProperty]
         public IFormFile Feltoltes { get; set; }
@@ -17,13 +20,42 @@ namespace FociWebapp.Pages
         }
         public async Task<IActionResult> OnPostAsync() {
 
+            if (Feltoltes == null || Feltoltes.Length == 0) {
+                ModelState.AddModelError("Feltoltes", "Adj meg egy állományt!");
+                return Page();
+            }
+
             var UploadDirPath = Path.Combine(_env.ContentRootPath, "uploads");
             var UploadFilePath = Path.Combine(UploadDirPath, Feltoltes.FileName);
 
             using (var stream = new FileStream(UploadFilePath, FileMode.Create))
             {
-                Feltoltes.CopyTo(stream);
+                await Feltoltes.CopyToAsync(stream);
             }
+
+            StreamReader sr = new StreamReader(UploadFilePath);
+            sr.ReadLine();
+
+            while (!sr.EndOfStream) {
+                var line = sr.ReadLine();
+                var elemek = line.Split(" ");
+
+                Meccs ujMeccs = new Meccs();
+                ujMeccs.Fordulo = int.Parse(elemek[0]);
+                ujMeccs.HazaiVeg = int.Parse(elemek[1]);
+                ujMeccs.VendegVeg = int.Parse(elemek[2]);
+                ujMeccs.HazaiFelido = int.Parse(elemek[3]);
+                ujMeccs.VendegFelido = int.Parse(elemek[4]);
+                ujMeccs.HazaiCsapat = elemek[5];
+                ujMeccs.VendegCsapat = elemek[6];
+
+                _context.Meccsek.Add(ujMeccs);
+            }
+
+            sr.Close();
+            _context.SaveChanges();
+
+            System.IO.File.Delete(UploadFilePath);
             return Page();
         }
     }
